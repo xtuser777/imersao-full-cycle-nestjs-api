6630328f7ac4a17598ee9bbe15df110913e5b930
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2024-02-01 14:30:05 -0300

    up

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: '123456',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthGuard],
+})
+export class AuthModule {}
