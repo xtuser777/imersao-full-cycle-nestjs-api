2b733c0b8d498a0b50f2be7b5d124752c583e4e6
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2024-02-02 18:50:38 -0300

    aula4

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/order-item.entity';
+import { Order } from './entities/order.entity';
 import { Product } from '../products/entities/product.entity';
+import { OrderConsumer } from './order.consumer';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order, OrderItem, Product])],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrderConsumer],
 })
 export class OrdersModule {}
